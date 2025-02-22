4d2d05744a1ba63556888b0a6677a50e3149be86
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-22 19:19:19 +0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -57,113 +57,8 @@ const Footer = () => {
                   Nime
                 </Text>
               </Link>
-            </Heading>
-
-            
-          </Box>
-
-          {/* Footer newsletter */}
-          <Box w={{ base: "100%", md: "40%" }} display="flex" flexDir="column">
-            <Heading
-              as="h3"
-              textTransform="uppercase"
-              mb="5px"
-              color="#fff"
-              fontSize={{ base: "20.11px", md: "27.54px" }}
-              fontWeight="500"
-              lineHeight={{ base: "24.2px", md: "33px" }}
-              letterSpacing="1.5px"
-              textAlign={{ base: "center", md: "start" }}
-            >
-              Get notified
-            </Heading>
-            <Text
-              as="p"
-              fontSize={{ base: "11.81px", md: "17.72px" }}
-              fontWeight="400"
-              lineHeight={{ base: "18px", md: "27px" }}
-              letterSpacing="0.5px"
-              color="#d4d4d4"
-              textAlign={{ base: "center", md: "start" }}
-            >
-              get emails on latest news about anime and more.
-            </Text>
-
-            <Box my="20px">
-              <Form>
-                <FormControl>
-                  <InputGroup>
-                    <Input
-                      placeholder="info@example.com"
-                      color="#fff"
-                      height="43px"
-                      _focus={{
-                        boxShadow: "0 0 5px 0 var(--accent-color)",
-                        borderColor: "var(--accent-color)",
-                      }}
-                      _hover={{
-                        outline: "none",
-                        borderColor: "var(--accent-color)",
-                        background: "none",
-                      }}
-                    />
-
-                    <InputRightAddon
-                      height="43px"
-                      background="none"
-                      w="141.38px"
-                    >
-                      <Button
-                        type="submit"
-                        w="100%"
-                        height="100%"
-                        background="none"
-                        color="#fff"
-                        fontSize={{ base: "11px", md: "20px" }}
-                        fontWeight="600"
-                        lineHeight={{ base: "18px", md: "30px" }}
-                        letterSpacing="1px"
-                        transition="all ease 0.25s"
-                        _hover={{
-                          background: "none",
-                          color: "var(--accent-color)",
-                        }}
-                      >
-                        Subscribe
-                      </Button>
-                    </InputRightAddon>
-                  </InputGroup>
-                </FormControl>
-              </Form>
-            </Box>
-
-            <Text
-              as="span"
-              fontSize="13.45px"
-              fontWeight="300"
-              lineHeight="21px"
-              letterSpacing="0.5px"
-              color="#d4d4d4"
-              textAlign={{ base: "center", md: "start" }}
-            >
-              By subscribing you agree to our terms and conditions.
-            </Text>
-          </Box>
-        </Box>
-      </Box>
-      <Box bg="var(--primary-background-color)" py="20px">
-        <Box
-          maxW={{
-            base: "95%",
-            xl: "85%",
-            "2xl": "container.xl",
-          }}
-          margin="auto"
-          display="flex"
-          flexDir={{ base: "column", md: "row" }}
-          alignItems="center"
-          justifyContent={{ base: "center", md: "space-between" }}
-        >
+            </Heading>      
+          </Box>          
           {/* Copyright */}
           <Box>
             <Text
@@ -175,7 +70,7 @@ const Footer = () => {
               letterSpacing="0.5px"
             >
               &copy;
-              {` ${new Date().getFullYear()} All rights reserved by AniPulse.`}
+              {` ${new Date().getFullYear()} All rights reserved by ArcleNime.`}
             </Text>
           </Box>
 
