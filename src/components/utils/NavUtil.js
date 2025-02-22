68c2606856fd2cf6523d52251a4c28288470f2c7
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-22 08:57:22 +0700

    Update NavUtil.js

--- src/components/utils/NavUtil.js
+++ src/components/utils/NavUtil.js
@@ -2,18 +2,5 @@ export const NavList = [
   {
     label: "Home",
     to: "/",
-  },
-  {
-    label: "Trending",
-    to: "/trending",
-  },
-  {
-    label: "Movies",
-    to: "/movies",
-  },
-
-  {
-    label: "Popular",
-    to: "/popular",
-  },
+  },  
 ];
