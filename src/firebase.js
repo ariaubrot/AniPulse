1484037376dae281036dc52e785bcaeaf0670cc5
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-22 08:45:41 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBowmGRXcQx1HQ0qRcXNflHydyRv6JGeCQ",
-  authDomain: "anipulse-e3979.firebaseapp.com",
+  apiKey: "AIzaSyDcWHuPrg1dlj8L-Ymnz1cYFn-Smal0pgM",
+  authDomain: "arclenime.firebaseapp.com",
   projectId: "anipulse-e3979",
-  storageBucket: "anipulse-e3979.appspot.com",
-  messagingSenderId: "384242062540",
-  appId: "1:384242062540:web:cf1bb76f4aceefe3081e69",
-  measurementId: "G-SZ0SH2CREH",
+  storageBucket: "arclenime.firebasestorage.app",
+  messagingSenderId: "729271912399",
+  appId: "1:729271912399:web:25d4ce93023bcfd530f4ba",
+  measurementId: "G-VQWX87M1XQ",
 };
 
 const app = initializeApp(firebaseConfig);
